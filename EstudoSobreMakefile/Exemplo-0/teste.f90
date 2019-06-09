986e94a77919dd4b9de7d412b67ad3f3ca501de7
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-06-09 15:44:32 -0300

    Exemplo de Compilação Simples com arquivo Makefile

---
+++ EstudoSobreMakefile/Exemplo-0/teste.f90
@@ -0,0 +1,15 @@
+PROGRAM teste
+!
+!       Objetivo: Exemplificar o uso dos arquivos Makefile
+!       na compilação de apenas um arquivo.
+!
+!       Programador: Rodolfo André Cardoso Neves (Dirack) 09/06/2019
+!
+!       Email (manutenção): rodolfo_profissional@hotmil.com
+!
+!       Site: http://www.dirackslounge.online
+IMPLICIT NONE
+
+        WRITE(*,*) "Exemplo Makefile simples rodando..."
+
+END PROGRAM

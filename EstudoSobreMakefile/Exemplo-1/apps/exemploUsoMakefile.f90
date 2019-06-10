c84b3dd9dad8b680b43fa2cbafe0c626f0fc97f3
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-06-09 23:00:41 -0300

    Refatoração do exemplo 1 compilar várias pastas com arquivos Makefile

--- EstudoSobreMakefile/Exemplo-1/apps/exemploUsoMakefile.f90
+++ EstudoSobreMakefile/Exemplo-1/apps/exemploUsoMakefile.f90
@@ -1,4 +1,20 @@
 PROGRAM exemploUsoMakefile
+!
+! exemploUsoMakefile.f90 (Fortran)
+! 
+! Objetivo: Exemplificar a compilação com arquivo Makefile a partir de várias 
+! pastas, utilizando as diretivas -o -J -I -c
+! 
+! Versão 1.0
+! 
+! Site: http://www.dirackslounge.online
+! 
+! Programador: Rodolfo A. C. Neves (Dirack) 09/06/2019
+! 
+! Email: rodolfo_profissional@hotmail.com
+! 
+! Licença: Software de uso livre e código aberto.
+
 USE math_lib
 USE input_lib
 USE moduloExemplo
--- EstudoSobreMakefile/Exemplo-1/src/moduloExemplo.f90
+++ EstudoSobreMakefile/Exemplo-1/src/moduloExemplo.f90
@@ -1,4 +1,20 @@
 MODULE moduloExemplo
+!
+! moduloExemplo.f90 (Fortran)
+! 
+! Objetivo: Pequena função para exemplificar a compilação.
+! Pede que o usuário insira seu nome.
+! 
+! Versão 1.0
+! 
+! Site: http://www.dirackslounge.online
+! 
+! Programador: Rodolfo A. C. Neves (Dirack) 09/06/2019
+! 
+! Email: rodolfo_profissional@hotmail.com
+! 
+! Licença: Software de uso livre e código aberto.
+
 IMPLICIT NONE
 
 CONTAINS

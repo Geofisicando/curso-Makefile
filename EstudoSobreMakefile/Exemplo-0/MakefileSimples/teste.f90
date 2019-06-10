2ca1f89282561ecd8deff557bae7e973ed88e1ef
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-06-09 22:44:50 -0300

    Refatoração do exemplo simples de makefile

--- EstudoSobreMakefile/Exemplo-0/MakefileSimples/teste.f90
+++ EstudoSobreMakefile/Exemplo-0/MakefileSimples/teste.f90
@@ -1,13 +1,18 @@
 PROGRAM teste
 !
-!       Objetivo: Exemplificar o uso dos arquivos Makefile
-!       na compilação de apenas um arquivo.
-!
-!       Programador: Rodolfo André Cardoso Neves (Dirack) 09/06/2019
-!
-!       Email (manutenção): rodolfo_profissional@hotmil.com
-!
-!       Site: http://www.dirackslounge.online
+! teste.f90 (Fortran)
+! 
+! Objetivo: Exemplificar o uso dos arquivos Makefile na compilação.
+! 
+! Versão 1.0
+! 
+! Site: http://www.dirackslounge.online
+! 
+! Programador: Rodolfo A. C. Neves (Dirack) 09/06/2019
+! 
+! Email: rodolfo_profissional@hotmail.com
+! 
+! Licença: Software de uso livre e código aberto.
 IMPLICIT NONE
 
         WRITE(*,*) "Exemplo Makefile simples rodando..."

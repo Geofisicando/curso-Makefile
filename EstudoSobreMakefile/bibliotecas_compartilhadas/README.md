839d0af969f16ff75f53a4fa8192937e2f7059e9
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-08-03 15:32:30 -0300

    Estudo sobre bibliotecas compartilhadas

---
+++ EstudoSobreMakefile/bibliotecas_compartilhadas/README.md
@@ -0,0 +1,6 @@
+#Estudo sobre Makefile
+
+>Nesta pasta coloquei o material sobre bibliotecas compartilhadas.
+
+Bibliotecas compartilhadas são códigos compilados para serem compartilhados 
+entre diversos programas diferentes. Eles são distribuídos como arquivos ”.so”
---
+++ EstudoSobreMakefile/bibliotecas_compartilhadas/principal.f90
@@ -0,0 +1,26 @@
+PROGRAM principal
+!
+! principal.f90 (Fortran 90)
+! 
+! Objetivo: Estudo sobre bibliotecas compartilhadas
+! 
+! Site: http://www.dirackslounge.online
+! 
+! Versão 1.0
+! 
+! Programador: Rodolfo Dirack 03/08/2019
+! 
+! Email: rodolfo_profissional@hotmail.com
+! 
+! Licença: GPL-3.0 <https://www.gnu.org/licenses/gpl-3.0.txt>.
+USE teste_1_lib
+USE teste_2_lib
+USE teste_3_lib
+
+IMPLICIT NONE
+
+	CALL mensagem_1()
+	CALL mensagem_2()
+	CALL mensagem_3()
+
+END PROGRAM

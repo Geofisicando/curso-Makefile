f73e59a81125fe763a702f39f60f21de8f26f9ac
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-08-18 14:01:10 -0300

    Debug de alguns arquivos das aulaas práticas

---
+++ EstudoSobreMakefile/Exemplo-0/MakefileSimples/teste/teste.f90
@@ -0,0 +1,20 @@
+PROGRAM teste
+!
+! teste.f90 (Fortran)
+! 
+! Objetivo: Exemplificar o uso dos arquivos Makefile na compilação.
+! 
+! Versão 1.0
+! 
+! Site: http://www.dirackslounge.online
+! 
+! Programador: Rodolfo A. C. Neves (Dirack) 09/06/2019
+! 
+! Email: rodolfo_profissional@hotmail.com
+! 
+! Licença: Software de uso livre e código aberto.
+IMPLICIT NONE
+
+        WRITE(*,*) "Exemplo Makefile simples rodando..."
+
+END PROGRAM
